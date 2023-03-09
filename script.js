6b564fcc908cc4dcde7150a99bc16507551a6698
Alan Canario <97064649+CanarioAlan@users.noreply.github.com>
2023-03-09 18:34:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const persona = `{"nombre":"Alan","apellido":"Canario", "ubicacion":"Santa Fe, Capital"}`;
+const obj = JSON.parse(persona);
+document.getElementById("titulo").innerHTML = obj.apellido + " " + obj.nombre;
+document.querySelector(".lugar").innerHTML = obj.ubicacion;
+
 document.getElementById("enviar").onclick = function () {
   document.getElementById("enviado").innerHTML = "formulario enviado";
 };
